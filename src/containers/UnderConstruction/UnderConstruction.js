c4f04190b4be76c36ea32a1ae706b79024f8f2d6
Matias Ellera <elleramatias@gmail.com>
2021-02-25 22:31:26 -0500

    minor under construction update

--- src/containers/UnderConstruction/UnderConstruction.js
+++ src/containers/UnderConstruction/UnderConstruction.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
 import Card from '../../components/UI/Card/Card';
 import classes from './UnderConstruction.module.scss';
+import Button from '../../components/UI/Button/Button';
+
 export class UnderConstruction extends Component {
   render() {
+    const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/matiasellera/';
+
+    const getInTouchClickHandler = () => {
+      window.open(LINKEDIN_PROFILE_URL, '_blank');
+    }
+
     return (
       <Card>
-        <div className="text-center mb-2">
+        <div className="text-center">
           Thank you for visiting! Unfortunately, this site is under construction for now. Stay tuned! <br />
-          In the meantime, enjoy this cat video:
         </div>
-        <div className={classes.video}>
-          <iframe width="500" height="385" src="https://www.youtube.com/embed/tpiyEe_CqB4" title="catVideo"></iframe>
+        <div className="text-center my-4">
+          <Button clicked={getInTouchClickHandler}>Get in touch</Button>
         </div>
       </Card>
     )
